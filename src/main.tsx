177b9952bbf2efa06c3aa9e407064268783ba3f9
Vivek Bigelow <vivek.bigelow@iga.in.gov>
2022-09-27 11:36:40 -0400

    Warning message

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import "./index.css";
 
+// Warning: StrictMode will render twice in dev mode. This catches subtle bugs.
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
   <BrowserRouter>
