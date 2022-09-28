39b2ed7bb5d7ec484c301d04794800ed68c745db
Vivek Bigelow <vivek.bigelow@iga.in.gov>
2022-09-28 14:36:12 -0400

    deleteFood and testing cleanup

--- cypress/e2e/Admin.cy.ts
+++ cypress/e2e/Admin.cy.ts
@@ -1,11 +1,22 @@
+import { deleteFood, searchFoods } from "../../src/services/foodsApi";
+
 describe('Admin', () => {
-  it('should submit a new Food and display it on the menu page', () => {
+  it('should submit a new Food and display it on the menu page', async () => {
+    // Delete any pre-existing records from a previous test
+    await searchFoods("BBQ Ribs").then((foods) => {
+      foods.forEach(async food => {
+        await deleteFood(food.id);
+      });
+    })
     cy.visit("http://127.0.0.1:5173");
     cy.findByRole("link", {name: "Admin"}).click();
     cy.findByLabelText("Name").type("BBQ Ribs");
     cy.findByLabelText("Description").type("Tangy and sweet");
     cy.findByLabelText("Price").type("19.99");
-    cy.findByLabelText("Image").type("fake-photo.jpg");
+    cy.findByLabelText("Image Filename").type("fake-photo.jpg");
     cy.findByLabelText("Lunch").check();
+    cy.findByRole("button", {name: "Save"}).click(),
+    cy.findByRole("link", {name: "Home"}).click(),
+    cy.findByRole("heading", {name: "BBQ Ribs"})
   })
 })
--- src/services/foodsApi.ts
+++ src/services/foodsApi.ts
@@ -7,6 +7,12 @@ export async function getFoods(): Promise<Food[]> {
   return resp.json();
 }
 
+export async function searchFoods(search: string): Promise<Food[]> {
+  const searchUrl = url + "?" + new URLSearchParams({ q: search }).toString();
+  const resp = await fetch(searchUrl);
+  return resp.json();
+}
+
 export async function addFood(food: NewFood): Promise<Food> {
   const resp = await fetch(url, {
     method: "POST",
@@ -14,6 +20,16 @@ export async function addFood(food: NewFood): Promise<Food> {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(food),
+  });
+  return resp.json();
+}
+
+export async function deleteFood(id: number): Promise<void> {
+  const resp = await fetch(url + '/' + id, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
   return resp.json();
 }
