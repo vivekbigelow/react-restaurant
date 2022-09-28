457f4650199601384206145f3d9fae35c93b6cb6
Vivek Bigelow <vivek.bigelow@iga.in.gov>
2022-09-28 11:46:04 -0400

    Add storybook

---
+++ src/resusable/Button.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Button from "./Button";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "resusable/Button",
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    className: { control: "string" },
+  },
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+    variant: "primary",
+    children: "Click me",
+    className: "custom-class"
+};
