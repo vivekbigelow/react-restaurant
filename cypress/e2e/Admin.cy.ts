c79e250a510dad04913b29facee84f686150bf99
Vivek Bigelow <vivek.bigelow@iga.in.gov>
2022-09-28 13:52:46 -0400

    Cypress Form test

--- cypress/e2e/Admin.cy.ts
+++ cypress/e2e/Admin.cy.ts
@@ -1,5 +1,11 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('Admin', () => {
+  it('should submit a new Food and display it on the menu page', () => {
     cy.visit("http://127.0.0.1:5173");
+    cy.findByRole("link", {name: "Admin"}).click();
+    cy.findByLabelText("Name").type("BBQ Ribs");
+    cy.findByLabelText("Description").type("Tangy and sweet");
+    cy.findByLabelText("Price").type("19.99");
+    cy.findByLabelText("Image").type("fake-photo.jpg");
+    cy.findByLabelText("Lunch").check();
   })
 })
